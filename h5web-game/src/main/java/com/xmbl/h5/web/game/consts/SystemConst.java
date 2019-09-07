90bdfa9fe3a0ef28862fcb9c2f9d131b344e2a73
noseparte <noseparte@aliyun.com>
2019-09-07 17:22:42 +0800

    SystemConst 系统参数

---
+++ h5web-game/src/main/java/com/xmbl/h5/web/game/consts/SystemConst.java
@@ -0,0 +1,9 @@
+package com.xmbl.h5.web.game.consts;
+
+public class SystemConst {
+	public static final String APPID = "wxcf772152e35e40**";
+	public static final String APPSECRET = "ef9c38a00f946f5b7fe0af8d19e893**";
+	
+	public static final String user_dir = System.getProperty("user.dir");
+	
+}
